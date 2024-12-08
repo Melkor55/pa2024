ff1c20aaf4b132375102a4c413267aeb5d312921
Melkor55 <alexhanc2000@yahoo.com>
2024-12-08 18:41:32 +0200

    commit 4, working version

--- firstTryOfTheAssignment.py
+++ firstTryOfTheAssignment.py
@@ -1,4 +1,4 @@
-from numba import njit, prange
+from numba import njit
 import numpy as np
 import time
 
@@ -23,33 +23,20 @@ def check_win(board, player):
         return True
     return False
 
-# Parallelized function for computer move
-@njit(parallel=True)
+# Function for computer move
+@njit
 def computer_move(board, player):
     N = board.shape[0]
     best_move = (-1, -1)
-    for i in prange(N):
+    for i in range(N):
         for j in range(N):
             if board[i, j] == 0:
                 board[i, j] = player
                 if check_win(board, player):
-                    best_move = (i, j)
+                    return i, j
                 board[i, j] = 0
-    return best_move
-
-
-# Player vs Computer game loop
-@njit(parallel=True)
-def computer_move(board, player):
-    N = board.shape[0]
-    best_move = (-1, -1)
-    for i in prange(N):
-        for j in range(N):
-            if board[i, j] == 0:
-                board[i, j] = player
-                if check_win(board, player):
+                if best_move == (-1, -1):
                     best_move = (i, j)
-                board[i, j] = 0
     return best_move
 
 # Player vs Computer game loop
@@ -82,45 +69,7 @@ def player_vs_computer(N):
             break
         print(board)
 
-
-
-# Computer vs Computer game loop
-def computer_vs_computer(N, debug=False):
-    board = initialize_board(N)
-    player1 = 1
-    player2 = 2
-    while True:
-        # Player 1 move
-        row, col = computer_move(board, player1)
-        if row != -1:
-            board[row, col] = player1
-        if row == -1:
-            if debug:
-                print(board)
-            break
-        if check_win(board, player1):
-            if debug:
-                print(board)
-            break
-        if debug:
-            print(board)
-        # Player 2 move
-        row, col = computer_move(board, player2)
-        if row != -1:
-            board[row, col] = player2
-        if row == -1:
-            if debug:
-                print(board)
-            break
-        if check_win(board, player2):
-            if debug:
-                print(board)
-            break
-        if debug:
-            print(board)
-
 # Main execution
 if __name__ == "__main__":
     N = 3  # Change this to test different board sizes
     player_vs_computer(N)
-    computer_vs_computer(N, debug=True)
